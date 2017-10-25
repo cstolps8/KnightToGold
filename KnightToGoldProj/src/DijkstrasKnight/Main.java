8c506846ff557aadde5672b88c34d41ddb12c546
bitantepd23 <31904620+bitantepd23@users.noreply.github.com>
2017-10-25 15:22:23 -0500

    Modified Main
    Added the gameObj creation and printBoard calls to the main class.

--- KnightToGoldProj/src/DijkstrasKnight/Main.java
+++ KnightToGoldProj/src/DijkstrasKnight/Main.java
@@ -3,8 +3,8 @@ package DijkstrasKnight;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Game gameObj = new Game();
+		gameObj.printBoardObj();
 	}
 
 }
